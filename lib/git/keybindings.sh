31326db5315f12ab7768fb6b2fc1c31245172d71
Scott Luther <scott@focist.co>
2025-12-10 16:49:18 -0500

    fix issues

--- lib/git/keybindings.sh
+++ lib/git/keybindings.sh
@@ -32,7 +32,7 @@ if [[ "$git_keyboard_shortcuts_enabled" = "true" ]]; then
 
       # Uses emacs style keybindings, so vi mode is not supported for now
       if ! set -o | grep -q '^vi .*on$'; then
-        if [[ $shell == "zsh" ]]; then
+        if breeze_shell_is "zsh"; then
           _bind "$git_commit_all_keys"              " git_commit_all""$RETURN_CHAR"
           _bind "$git_add_and_commit_keys"          " \033[1~ git_add_and_commit ""$RETURN_CHAR"
           _bind "$git_commit_all_with_ci_skip_keys" " \033[1~ GIT_COMMIT_MSG_SUFFIX='[ci skip]' git_commit_all ""$RETURN_CHAR"
